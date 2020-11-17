45604c5f020543f41cd91dedbe8ae56773ab05f0
Ana Carolina Cortez Alves <carolina.cortez@aluno.ifsp.edu.br>
2020-11-17 19:18:14 -0300

    finally

--- trycatchcontasbytebank/Conexao.java
+++ trycatchcontasbytebank/Conexao.java
@@ -8,6 +8,7 @@ public class Conexao {
 
     public void leDados() {
         System.out.println("Recebendo dados");
+        throw new IllegalStateException();
     }
 
     public void fecha() {
--- trycatchcontasbytebank/TesteConexao.java
+++ trycatchcontasbytebank/TesteConexao.java
@@ -3,5 +3,14 @@ package trycatchcontasbytebank;
 public class TesteConexao {
     public static void main(String[] args) {
 
+        Conexao conexao = new Conexao();
+
+        try{
+            conexao.leDados();
+        } catch (IllegalStateException erro){
+            System.out.println("Deu erro na conexão: " + erro.getMessage());
+        } finally {
+            conexao.fecha();
+        }
     }
 }
