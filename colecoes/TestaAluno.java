781b09baa78805d39eb99f1c9a50d2d552d944d3
Ana Carolina Cortez Alves <carolina.cortez@aluno.ifsp.edu.br>
2020-11-18 22:07:37 -0300

    set

---
+++ colecoes/TestaAluno.java
@@ -0,0 +1,25 @@
+package colecoes;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class TestaAluno {
+
+    public static void main(String[] args){
+        Set<String> alunos = new HashSet<>();
+        //não aceita repetição, porém, a ordem de gravação dos registros é variada
+
+        alunos.add("Tinky Winky");
+        alunos.add("Dipsy");
+        alunos.add("Lala");
+        alunos.add("Po");
+
+        alunos.forEach(aluno -> System.out.println(aluno));
+
+        boolean isPoMatriculado = alunos.contains("Po");
+        System.out.println(isPoMatriculado);
+
+        //Collections.sort não funciona para Set, somente para List.
+        //List<String> alunosEmLista = new ArrayList<>(alunos); => cria uma lista do set alunos, assim poderia usar sort
+    }
+}
