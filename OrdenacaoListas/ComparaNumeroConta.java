db51cf3037602bb3da9186fa53bb6050630d53ba
Ana Carolina Cortez Alves <carolina.cortez@aluno.ifsp.edu.br>
2020-11-20 16:37:23 -0300

    refactor

--- OrdenacaoListas/ComparaNumeroConta.java
+++ OrdenacaoListas/ComparaNumeroConta.java
@@ -8,13 +8,6 @@ public class ComparaNumeroConta implements Comparator<Conta> {
 
     @Override
     public int compare(Conta c1, Conta c2) {
-        if(c1.getNumero() < c2.getNumero()) {
-            return -1;
-        }
-
-        if(c1.getNumero() > c2.getNumero()) {
-            return 1;
-        }
-        return 0;
+        return c1.getNumero() - c2.getNumero();
     }
 }
